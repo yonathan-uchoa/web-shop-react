04701f5ff09d5b4c2fb12308d901c557ba183400
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2024-03-12 23:10:50 -0400

    feat: add modal to product on click, add product dispatch

--- src/components/StoreItem/index.tsx
+++ src/components/StoreItem/index.tsx
@@ -1,48 +1,66 @@
 import { Button, Card } from "react-bootstrap";
-import { useContext } from "react";
-import { ShoppingCartContext } from "../../context/ShoppingCart/context";
-import { increaseCartQuantity } from "../../context/ShoppingCart/actions.ts";
+import { useContext, useState } from "react";
+import {
+  CartContextType,
+  ShoppingCartContext,
+} from "../../context/ShoppingCart/context";
+import {
+  addItemCart,
+  removeItemCart,
+} from "../../context/ShoppingCart/actions.ts";
 
 import "./styles.scss";
+import { ItemFrame } from "../ItemFrame/index.tsx";
+import { Product } from "../../context/ShoppingCart/data.ts";
 
-export type StoreItemsProps = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: { rate: number; count: number };
-};
+export const StoreItem = (item: Product) => {
+  const cartContext = useContext(ShoppingCartContext) as CartContextType;
+  const { cartState, cartDispatch } = cartContext;
+  const itemOnCart = cartState.products.find((el) => el.id === item.id);
 
-export const StoreItem = (item: StoreItemsProps) => {
-  const cartContext = useContext(ShoppingCartContext);
-  const { cartState, cartDispatch }: any = cartContext;
+  const [show, setShow] = useState(false);
 
   return (
-    <Card className="item-card">
-      <Card.Img
-        variant="top"
-        src={item.image}
-        height="150px"
-        style={{ objectFit: "contain" }}
-      />
-      <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4 item-title">
-          <span className="fs-5 text-truncate">{item.title}</span>
-          <span className="ms-2 text-muted">${item.price}</span>
-        </Card.Title>
-        <div className="mt-auto">
-          <Button
-            onClick={() => {
-              increaseCartQuantity(cartDispatch, item);
-            }}
-            className="w-100"
+    <>
+      <ItemFrame item={item} setShow={setShow} show={show} />
+      <Card className="item-card">
+        <Card.Img
+          variant="top"
+          src={item.image}
+          height="150px"
+          style={{ objectFit: "contain", cursor: "pointer" }}
+          onClick={() => setShow(true)}
+        />
+        <Card.Body className="d-flex flex-column">
+          <Card.Title
+            className="d-flex justify-content-between align-items-baseline mb-4 item-title"
+            onClick={() => setShow(true)}
           >
-            +Add To Cart
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+            <span className="fs-5 text-truncate">{item.title}</span>
+            <span className="ms-2 text-muted">${item.price}</span>
+          </Card.Title>
+          <div className="mt-auto">
+            {itemOnCart ? (
+              <Button
+                variant="danger"
+                onClick={() => removeItemCart(cartDispatch, item)}
+                className="w-100"
+              >
+                Remove from Cart
+              </Button>
+            ) : (
+              <Button
+                onClick={() => {
+                  addItemCart(cartDispatch, item);
+                }}
+                className="w-100"
+              >
+                +Add To Cart
+              </Button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
