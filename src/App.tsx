e1f04bc5c7a6d7bd80ffb8687df97fd8578ff8ac
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2024-03-14 22:31:03 -0400

    style: give some style to our page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCart";
 import { ShoppingCart } from "./components/ShoppingCart";
+import "./styles.scss";
 
 function App() {
   return (
     <>
       <ShoppingCartProvider>
         <Navbar />
-        <Container className="mb-4">
+        <Container className="mb-4 bg-white main-container">
           <Outlet />
         </Container>
         <ShoppingCart />
