73cc0401ebbc437aeb95f6d20be5cf350bc62736
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2024-03-14 22:30:25 -0400

    feat: create Welcome /home text

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,36 @@
+import { Container } from "react-bootstrap";
+import "./styles.scss";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Container className="home-container">
+      <h1>The web-shop is alive! </h1>
+      <p>
+        This project its simple, he pick some items to a Cart and generate a
+        Order log when it is finished. But he doesn't have a official payment
+        method - like PayPal -, the project itself is just for a bit more
+        practice whit React, Express and TS.
+      </p>
+      <p>
+        For the full functionalities of the project, you will need to check if
+        the <i>server</i> is online. The server is up in a{" "}
+        <b>
+          <a href="https://webshop-backend.adaptable.app/" target="_blank">
+            Adaptable server
+          </a>
+        </b>{" "}
+        - free tier, he will go up automatically in the first http request!
+      </p>
+      <p>
+        And here is the{" "}
+        <b>
+          <a href="https://github.com/yonathan-uchoa/web-shop" target="_blank">
+            GitHub repository
+          </a>
+        </b>{" "}
+      </p>
+    </Container>
+  );
 };
 
 export default Home;
