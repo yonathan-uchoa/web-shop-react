6e5472e8ca12cf926f85bbb307acaebce9ab6152
Yonathan Uchoa S. Kaveski <yonathan.uchoa@gmail.com>
2024-03-15 22:28:46 -0400

    feat: add category filter to item show in /store

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,34 @@
 import { Form, Stack } from "react-bootstrap";
 import "./styles.scss";
 
-export const Sidebar = ({ categories }: { categories: string[] }) => {
-  console.warn(categories);
+type Props = {
+  setCategory: React.Dispatch<React.SetStateAction<Set<string>>>;
+  categories: string[];
+};
+
+export const Sidebar = ({ categories, setCategory }: Props) => {
+  const addFilter = (isChecked: boolean, filter: string) => {
+    if (isChecked) {
+      setCategory((prev) => new Set(prev).add(filter));
+    } else {
+      setCategory((prev) => {
+        const res = new Set(prev);
+        res.delete(filter);
+        return res;
+      });
+    }
+  };
+
   return (
     <Stack direction="horizontal" gap={2} className="d-flex">
       <Form className="sidebar-form w-100">
         {categories.map((category, index) => (
-          <Form.Check type="checkbox" key={index} label={category} />
+          <Form.Check
+            key={index}
+            onChange={(e) => addFilter(e.target.checked, category)}
+            type="checkbox"
+            label={category}
+          ></Form.Check>
         ))}
       </Form>
     </Stack>
--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -4,6 +4,7 @@ import { Sidebar } from "../../components/Sidebar";
 import { useFetch } from "../../hooks/useFetch";
 import "./styles.css";
 import { Product } from "../../context/ShoppingCart/data";
+import { useState } from "react";
 
 export const Store = () => {
   const [products, productsLoading] = useFetch(
@@ -14,19 +15,35 @@ export const Store = () => {
     "https://webshop-backend.adaptable.app/products/categories",
     {}
   );
+  const [categoryFilter, setCategoryFilter] = useState<Set<string>>(
+    new Set<string>()
+  );
+  let filterProd: Product[] = [];
 
-  if (productsLoading || categoriesLoading) return <p>Loading...</p>;
+  if (productsLoading || categoriesLoading) {
+    return <p>Loading...</p>;
+  } else {
+    categoryFilter.size === 0
+      ? (filterProd = products)
+      : (filterProd = products.filter((p: Product) =>
+          categoryFilter.has(p.category)
+        ));
+  }
+
   return (
     <>
       {products && categories ? (
         <Container fluid>
           <Row>
             <Col sm={3} className="text-break side-bar bg-white">
-              <Sidebar categories={categories} />
+              <Sidebar
+                categories={categories}
+                setCategory={setCategoryFilter}
+              />
             </Col>
             <Col sm={9}>
               <Row md={2} xs={1} lg={3} className="g-3">
-                {products.map((item: Product) => {
+                {filterProd.map((item: Product) => {
                   return (
                     <Col key={item.id}>
                       <StoreItem {...item} />
